47f0e6a2860bc828467bb03d9ae56152cb39b393
cunderwoodmn@gmail.com <student@epicodus.com>
2015-08-21 12:33:23 -0700

    added code to stylist class and spec to pass #all

--- lib/stylist.rb
+++ lib/stylist.rb
@@ -1,3 +1,19 @@
 class Stylist
+  attr_reader(:stylist_name, :stylist_id)
 
+    define_method(:initialize) do |attributes|
+      @stylist_name = attributes.fetch(:stylist_name)
+      @stylist_id = attributes.fetch(:stylist_id)
+    end
+
+    define_singleton_method(:all) do
+      returned_stylists = DB.exec("Select * FROM stylists;")
+      stylists = []
+      returned_stylists.each() do |stylist|
+        client_name = stylist.fetch("client_name")
+        stylist_id = stylist.fetch("stylist_id").to_i()
+        stylists.push(Stylist.new({:stylist_name => stylist_name, :stylist_id => stylist_id}))
+      end
+      stylists
+    end
 end
--- spec/stylist_spec.rb
+++ spec/stylist_spec.rb
@@ -1,1 +1,9 @@
 require('spec_helper')
+
+describe(Stylist) do
+  describe(".all") do
+    it("starts off with no stylist") do
+      expect(Stylist.all()).to(eq([]))
+    end
+  end
+end
